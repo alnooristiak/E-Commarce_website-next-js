ef6c9498028d7b7424008eb934e0779e4a903e1e
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-01-26 00:45:15 +0600

    find an error setto cart value is 0

--- pages/checkout.js
+++ pages/checkout.js
@@ -96,10 +96,10 @@ const Checkout = ({ cart, addToCart, removeToCart, clearCart, subTotal }) => {
                             })
                         }
                     </ol>
-                    <Link href={"/checkout"}>
-                        <button className="flex text-center text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Check Out</button>
-                    </Link>
-                    <button onClick={clearCart} className="flex my-2 text-center text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Clear Cart</button>
+                    {/* <button onClick={clearCart} className="flex my-2 text-center text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">subTotal{subTotal}</button> */}
+                    <p className='font-bold my-4 text-white p-2 bg-indigo-500 text-2xl'>
+                        <span>Sub Total:  {subTotal}</span>
+                    </p>
                 </div>
             </div>
         </div>
